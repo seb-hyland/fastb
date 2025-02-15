2e201529b19946622b1b1a1968e5bd7f786e1c71
Sebastian Hyland <st.hyland05@gmail.com>
2025-02-15 03:15:23 -0800

    feat(sequences): sequence type construction macro, sequence types

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+mod sequences;
 
 struct FileHeader {
     seq_type: u8,
@@ -6,7 +7,7 @@ struct FileHeader {
 
 impl FileHeader {
     fn to_u8(&self) -> u8 {
-        self.seq_type << 7 | self.checksum  
+        (self.seq_type << 7) | self.checksum  
     }
 }
 
@@ -15,32 +16,3 @@ struct SequenceHeader {
     desc: String,
 }
 
-enum NAcidMin {
-    A,
-    C,
-    G,
-    TU,
-}
-
-impl NAcidMin {
-    fn from_char(c: char) -> Result<Self, char> {
-        match c.to_ascii_lowercase() {
-            'a' => Ok(Self::A),
-            'c' => Ok(Self::C),
-            'g' => Ok(Self::G),
-            't' | 'u' => Ok(Self::TU),
-            // Padded chars are set as 0b10
-            '^' => Ok(Self::G),
-            _ => Err(c),
-        }
-    }
-
-    fn to_u8(&self) -> u8 {
-        match self {
-            Self::A => 0b00,
-            Self::C => 0b01,
-            Self::G => 0b10,
-            Self::TU => 0b11,
-        } 
-    }
-}
---
+++ src/sequences.rs
@@ -0,0 +1,105 @@
+trait Sequence {
+    fn from_char(c: char) -> Result<Self, char> where Self: Sized;
+}
+
+macro_rules! define_sequence_type {
+    ($name:ident { $($variant:ident : $( $char:literal ),+ => $value:expr ),* }) => {
+        #[derive(Debug, Clone, Copy, PartialEq)]
+        enum $name {
+            $($variant),*
+        }
+
+        impl Sequence for $name {
+            fn from_char(c: char) -> Result<Self, char> {
+                match c.to_ascii_lowercase() {
+                    $( $( $char => Ok(Self::$variant), )+ )*
+                    _ => Err(c),
+                }
+            }
+        }
+
+        impl From<$name> for u8 {
+            fn from(value: $name) -> Self {
+                match value {
+                    $( $name::$variant => $value, )*
+                }
+            }
+        }
+
+        impl std::fmt::Display for $name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                match self {
+                    $( Self::$variant => write!(f, stringify!($variant)), )*
+                }
+            }
+        }
+
+        impl std::convert::TryFrom<u8> for $name {
+            type Error = u8;
+
+            fn try_from(value: u8) -> Result<Self, Self::Error> {
+                match value {
+                    $( $value => Ok(Self::$variant), )*
+                    _ => Err(value),
+                }
+            }
+        }
+    };
+}
+
+define_sequence_type!(NucleicAcid {
+    Adenine : 'a' => 0b00,
+    Cytosine : 'c' => 0b01,
+    Guanine : 'g', '^' => 0b10,
+    ThymineUracil : 't', 'u' => 0b11
+});
+
+define_sequence_type!(NucleicAcidExpanded {
+    Adenine : 'a' => 0b0001,
+    Cytosine : 'c' => 0b0010,
+    Guanine : 'g' => 0b0100,
+    ThymineUracil : 't', 'u' => 0b1000,
+    Purine : 'r' => 0b0101,
+    Pyrimidine : 'y', '^' => 0b1010,
+    Ketone : 'k' => 0b1100,
+    AminoGroup : 'm' => 0b0011,
+    StrongInteraction : 's' => 0b0110,
+    WeakInteraction : 'w' => 0b1001,
+    NotA : 'b' => 0b1110,
+    NotC : 'd' => 0b1101,
+    NotG : 'h' => 0b1011,
+    NotTU : 'v' => 0b0111,
+    NucleicAcid : 'n' => 0b1111,
+    Gap : '-' => 0b0000
+});
+
+define_sequence_type!(AminoAcid {
+    Alanine : 'a' => 0b00000,
+    DorN : 'b' => 0b00001,
+    Cysteine : 'c' => 0b00010,
+    AsparticAcid : 'd' => 0b00011,
+    GlutamicAcid : 'e' => 0b00100,
+    Phenylalanine : 'f' => 0b00101,
+    Glycine : 'g' => 0b00110,
+    Histidine : 'h' => 0b00111,
+    Isoleucine : 'i' => 0b01000,
+    IorL : 'j' => 0b01001,
+    Lysine : 'k' => 0b01010,
+    Leucine : 'l' => 0b01011,
+    Methionine : 'm' => 0b01100,
+    Asparagine : 'n' => 0b01101,
+    Pyrrolysine : 'o' => 0b01110,
+    Proline : 'p' => 0b01111,
+    Glutamine : 'q' => 0b10000,
+    Arginine : 'r' => 0b10001,
+    Serine : 's' => 0b10010,
+    Threonine : 't' => 0b10011,
+    Selenocysteine : 'u' => 0b10100,
+    Valine : 'v', '^' => 0b10101,
+    Tryptophan : 'w' => 0b10110,
+    Tyrosine : 'y' => 0b10111,
+    EorQ : 'z' => 0b11000,
+    Any : 'x' => 0b11001,
+    Stop : '*' => 0b11010,
+    Gap : '-' => 0b11011
+});
